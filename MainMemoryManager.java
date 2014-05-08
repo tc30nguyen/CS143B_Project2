b782adac1b13397c0529339601248eb6b34b8f69
TC <tc30nguyen@gmail.com>
2014-05-08 01:01:01 -0700

    started mm_release function

--- MainMemoryManager.java
+++ MainMemoryManager.java
@@ -15,23 +15,66 @@ public class MainMemoryManager
 		return mem_size;
 	}
 
+	//returns -1 for no fit
 	public int mm_request(int n)
 	{
 		//request block of n consecutive words
 		//return index of first usable word or error if insufficient mem
 	}
 
-	public int mm_release()
+	public int mm_release(int index)
 	{
 		//releases previously requested block back to mm
+
+		//goto index
+		//if left is hole (or tag(s) only), combine them
+		//if right is hole (or tag(s) only), combine them
+
+		int counter = 0;
+		int currentIndex = index - 1;
+		if(currentIndex >= 0 && mainMemory[currentIndex] == 0)
+		{
+			while(mainMemory[currentIndex] == 0)
+			{
+				currentIndex -= 1;
+				counter++;
+			}
+		}
+
+		//combine left
+		if(currentIndex >= 0 && mainMemory[currentIndex] < 0)
+		{
+			counter += 1;
+			int toMove = mainMemory[currentIndex] * -1 + 1;
+			mainMemory[currentIndex] = 0;
+			currentIndex -= toMove;
+			counter += toMove;
+
+			mainMemory[currentIndex] = counter;
+			currentIndex += counter + 1;
+		}
+
+		//combine right
+		if(currentIndex < mainMemory.length)
+		{
+			if(currentIndex == mainMemory.length - 1)
+				mainMemory[currentIndex] = counter;
+			else
+			{
+				if()
+				{
+					
+				}
+			}
+		}
 	}
 
-	private firstFit(int n)
+	//iterates through memory array, giving the request the first block that fits. Returns -1 for no fit
+	private int firstFit(int n)
 	{
-		boolean placed = false;
-		for(int i = 0; i < mainMemory.length && !placed; i++)
+		for(int i = 0; i < mainMemory.length; i++)
 		{
-			if(mainMemory[i] * -1 >= n)
+			if(mainMemory[i] * -1 >= n) //current hole can fit n
 			{
 				int difference = mainMemory[i] + n + 2; //calculate remaining space
 				int rightSide = 1 + i + n;
@@ -40,11 +83,11 @@ public class MainMemoryManager
 
 				if(difference == 2) //if n perfectly fits the hole, do nothing
 				{ }
-				else if (difference == 1 || difference == 0) 
+				else if(difference == 1 || difference == 0) 
 				{
-					mainMemory[rightSide + 1] = 1; //sets size 1 hole to 1, denoting that it cannot hold anything due to lack of space for both tags and data
+					mainMemory[rightSide + 1] = 0; //sets size 1 hole to 0, denoting that it cannot hold anything due to lack of space for both tags and data
 					if (difference == 0) 
-						mainMemory[rightSide + 2] = 1; //sets size 1 hole to 1, denoting that it cannot hold anything due to lack of space for both tags and data
+						mainMemory[rightSide + 2] = 0; //sets size 2 hole to 0, denoting that it cannot hold anything due to lack of space for both tags and data
 				}
 				else //retag the original hole with its decreased size
 				{
@@ -52,8 +95,17 @@ public class MainMemoryManager
 					mainMemory[rightSide + 2 - difference] = difference;
 				}
 
-				placed = true;
+				return i;
+			}
+			else //move the iterator to the next memory block
+			{
+				int move = mainMemory[i] > 0 ? mainMemory[i] : mainMemory[i] * -1;
+				i += move + 1;
 			}
 		}
+
+		return -1;
 	}
+
+
 }
