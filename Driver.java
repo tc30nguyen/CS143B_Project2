885e7e1ca4abbf40fbb158619fac4eec34f5085a
TC <tc30nguyen@gmail.com>
2014-05-10 01:06:58 -0700

    started the runSim function

--- Driver.java
+++ Driver.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.io.File;
+import java.util.Random;
 
 public class Driver 
 {
@@ -8,7 +9,7 @@ public class Driver
 	int mem_size;
 	int a; //average of gaus fnc
 	int d; //standard deviation of gaus fnc
-	int sim_step; //how many requests/releases to run
+	final int sim_step = 10000; //how many requests/releases to run
 	int total_search_time;
 	LinkedList<Integer> allocatedBlocks;
 	Strategy strategy;
@@ -18,11 +19,40 @@ public class Driver
 		mem_utilization = 0.0;
 		output = new File("output");
 		this.mem_size = mem_size;
-		this.a = a;
-		this.d = d;
-		this.sim_step = sim_step;
+		a = 0;
+		d = 0;
 		total_search_time = 0;
 		allocatedBlocks = new LinkedList<>();
+		strategy = null;
+	}
+	
+	public void runSim(int a, int d, Strategy s)
+	{
+		this.a = a;
+		this.d = d;
 		strategy = s;
+		
+		//for sim_step, run request/release
+		//get avg mem utilization & search time
+		for(int i = 0; i < sim_step; i++)
+		{
+			int index = -1;
+			do
+			{
+				index = mm_request(getRand); //request must count # holes examined for avg search
+				if(index != -1)
+					allocatedBlocks.add(index);
+			}
+			while(index != -1)
+
+			//record mem utilization: add block sizes, divide by total mem size. computer average for all iterations
+			//select block p tfrom allocatedBlocks to be released
+			//mm_release(p):
+		}
+	}
+	
+	private int getRand(int avg, int stdDev)
+	{
+		return 0;
 	}
 }
