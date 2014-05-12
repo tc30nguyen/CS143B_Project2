ed6a1005964171b935c13a56ee4766e4156933ea
TC <tc30nguyen@gmail.com>
2014-05-12 13:29:33 -0700

    test cases

--- Initializer.java
+++ Initializer.java
@@ -1,9 +1,19 @@
-
 public class Initializer {
 
 	public static void main(String[] args) 
 	{
+		Driver driver = new Driver(20);
+		driver.runSim(1, 3, Strategy.FIRST_FIT);
+		driver.runSim(5, 3, Strategy.FIRST_FIT);
+		driver.runSim(3, 5, Strategy.FIRST_FIT);
+		driver.runSim(3, 8, Strategy.FIRST_FIT);
 		
+		driver.runSim(1, 3, Strategy.BEST_FIT);
+		driver.runSim(5, 3, Strategy.BEST_FIT);
+		driver.runSim(3, 5, Strategy.BEST_FIT);
+		driver.runSim(3, 8, Strategy.BEST_FIT);
+		
+		driver.outputResults();
 	}
 
 }
