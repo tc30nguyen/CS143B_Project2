523a1181339d944631261b9835154fa801dbbb3c
TC <tc30nguyen@gmail.com>
2014-05-12 22:23:41 -0700

    test case for submission

--- Initializer.java
+++ Initializer.java
@@ -2,16 +2,16 @@ public class Initializer {
 
 	public static void main(String[] args) 
 	{
-		Driver driver = new Driver(20);
-		driver.runSim(1, 3, Strategy.FIRST_FIT);
-		driver.runSim(5, 3, Strategy.FIRST_FIT);
-		driver.runSim(3, 5, Strategy.FIRST_FIT);
-		driver.runSim(3, 8, Strategy.FIRST_FIT);
+		Driver driver = new Driver(10000);
+		driver.runSim(20, 2000, Strategy.FIRST_FIT);
+		driver.runSim(3000, 2000, Strategy.FIRST_FIT);
+		driver.runSim(1000, 1000, Strategy.FIRST_FIT);
+		driver.runSim(1000, 3000, Strategy.FIRST_FIT);
 		
-		driver.runSim(1, 3, Strategy.BEST_FIT);
-		driver.runSim(5, 3, Strategy.BEST_FIT);
-		driver.runSim(3, 5, Strategy.BEST_FIT);
-		driver.runSim(3, 8, Strategy.BEST_FIT);
+		driver.runSim(20, 2000, Strategy.BEST_FIT);
+		driver.runSim(3000, 2000, Strategy.BEST_FIT);
+		driver.runSim(1000, 1000, Strategy.BEST_FIT);
+		driver.runSim(1000, 3000, Strategy.BEST_FIT);
 		
 		driver.outputResults();
 	}
